2486e8d4346dfb70529fb81666fe9a7ceba392c2
root <root@ip-172-31-40-83.ec2.internal>
2024-08-16 12:35:49 +0000

    added number.java

---
+++ number.java
@@ -0,0 +1,17 @@
+public class  login user
+{
+	public static viod main (string args[])
+
+	{
+		int i;
+		for(i=0;i<=38;i++){
+
+
+			system.out.println("number: +i);
+		
+		
+
+		
+		                                                                                                                                              
+		
+		}
